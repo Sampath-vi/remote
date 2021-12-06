a06d456efcea7e118efa0cd5c948bff4fd43e970
sampath <sampathvinayak1453@gmail.com>
2021-12-07 05:29:12 +0530

    show posts on save

--- src/components/addNewPost.tsx
+++ src/components/addNewPost.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import postStore, { Post } from '../stores/postStore';
 import { observer } from 'mobx-react';
 import "../index.css";
+import { Modal } from 'antd';
 
 interface AddNewPost {
   post: Post;
@@ -14,17 +15,31 @@ export default observer(({ post: { title = "", body = "" } = { title: "", body: 
 
   const [postTitle, setPostTitle] = React.useState(title);
   const [postBody, setPostBody] = React.useState(body);
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
 
-  const submitForm = async (e: React.MouseEvent) => {
-    e.preventDefault();
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = async () => {
+    await submitForm();
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+
+  const submitForm = async () => {
     await postStore.addPost({
       title: postTitle,
       body: postBody,
     });
   };
 
-  const onFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+  const onFormChange = (e: React.ChangeEvent<HTMLElement>) => {
+    const { name, value } = e.target as HTMLInputElement;
     if (name === "title") {
       onChange({ title: value, body: postBody });
     } else if (name === "body") {
@@ -32,11 +47,10 @@ export default observer(({ post: { title = "", body = "" } = { title: "", body: 
     }
   };
 
-
   return (
     <div className="container">
-      <h2>Add New Post</h2>
-      <form>
+      <button onClick={showModal}>Add New Post</button>
+      <Modal title="New Post" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
         <div style={{ marginBottom: '10px' }}>
           <label>Title:  </label>
           <input type="text" name="title" value={postTitle} onChange={(e) => {
@@ -47,14 +61,23 @@ export default observer(({ post: { title = "", body = "" } = { title: "", body: 
 
         <div style={{ marginBottom: '20px' }}>
           <label>Body:  </label>
-          <input type="text" name="body" value={postBody} onChange={(e) => {
+          <textarea name="body" value={postBody} onChange={(e) => {
             onFormChange(e);
             setPostBody(e.target.value);
           }} />
         </div>
+      </Modal>
 
-        <button onClick={submitForm} value="Submit">Submit</button>
-      </form>
+
+      <h2>Posts</h2>
+      {postStore.posts != null && postStore.posts.map((post, index) => {
+        return (
+          <div key={index} style={{border: '1px solid grey', borderRadius: '6px', backgroundColor: '#F1F2F3'}}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        )
+      })}
     </div>
   );
 });
--- src/stores/postStore.ts
+++ src/stores/postStore.ts
@@ -9,16 +9,19 @@ export interface Post {
 interface PostStore {
   post: Post | null;
   addPost: (u: Post) => void;
+  posts: Post[] | null;
 }
 
 class postStore implements PostStore {
 
   post = null;
+  posts = JSON.parse(localStorage.getItem('posts')) || [];
 
 
   constructor() {
     makeObservable(this, {
       post: observable,
+      posts: observable,
       addPost: action
     })
   }
@@ -27,6 +30,8 @@ class postStore implements PostStore {
     try {
       const resp = await axios.post('https://jsonplaceholder.typicode.com/posts', { ...post, userId: Math.random() });
       this.post = resp.data;
+      this.posts.push(this.post);
+      localStorage.setItem('posts', JSON.stringify(this.posts));
     } catch (error) {
 
     }
